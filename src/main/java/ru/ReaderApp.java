a5b9f03caf6323d7916525a01e6753c8d2e85771
3litra <efanovalva@gmail.com>
2020-10-29 22:12:41 +0500

    task 04

--- src/main/java/ru/ReaderApp.java
+++ src/main/java/ru/ReaderApp.java
@@ -5,6 +5,8 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static com.sun.xml.internal.ws.util.VersionUtil.compare;
+
 /**
  * Задание 1. Написать программу, читающую текстовый файл. Программа должна составлять отсортированный по алфавиту
  * список слов, найденных в файле и сохранять его в файл-результат. Найденные слова не должны повторяться, регистр
@@ -26,12 +28,20 @@ import java.util.regex.Pattern;
  */
 public class ReaderApp {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        ArrayList<String> list = readTxtFile();
-        TreeSet<String> sortedSet = sortTxtFile(list);
-        writeTxtFile(sortedSet);
+    public static void main(String[] args)  {
+        try{
+            ArrayList<String> list = readTxtFile();
+            TreeSet<String> sortedSet = sortTxtFile(list);
+            writeTxtFile(sortedSet);
+        }catch (FileNotFoundException e){
+            e.getStackTrace();
+        }
     }
 
+    /**
+     * Метод для записи отсоритрованного текста в файл .txt , если файл уже записывался, он будет перезаписан
+     * @param sortedSet принимаемый аргумент - TreeSet отсортированных слов
+     */
     private static void writeTxtFile(TreeSet<String> sortedSet) {
         try(FileWriter writer = new FileWriter("SortedTask04.txt", false))
         {
@@ -45,19 +55,13 @@ public class ReaderApp {
         }
     }
 
+    /**
+     * Метод для записи считанных слов из исходного файла  их в TreeSet и их сортировки
+     * @param list принимаемый аргумент - ArrayList созданный из слов считанных из исходного файла
+     * @return возвращаемый аргумент - TreeSet отсортированных слов
+     */
     private static TreeSet<String> sortTxtFile(ArrayList<String> list) {
-        Comparator<String> comparator = new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                int stringCompare = o1.compareToIgnoreCase(o2);
-
-                if (stringCompare != 0) {
-                    return stringCompare;
-                } else {
-                    return 0;
-                }
-            }
-        };
+        Comparator<String> comparator = (String string1, String string2) -> string1.compareToIgnoreCase(string2) ;
         TreeSet<String> sortedSet = new TreeSet<>(comparator);
         Pattern pattern = Pattern.compile("[а-яА-яa-zA-Z]+\\b");
         for (int i = 0; i < list.size(); i++) {
@@ -69,6 +73,11 @@ public class ReaderApp {
         return sortedSet;
     }
 
+    /**
+     * Метод для считавания и записи в ArrayList слов из исходного файла
+     * @return  возвращаемы параметр - ArrayList слов из исходного текста
+     * @throws FileNotFoundException исключение появится если будет не найден исходный файл для считывания
+     */
     private static ArrayList<String> readTxtFile() throws FileNotFoundException {
         ArrayList<String> list = new ArrayList<>();
         Scanner scanner = new Scanner(new File("task04.txt"));
