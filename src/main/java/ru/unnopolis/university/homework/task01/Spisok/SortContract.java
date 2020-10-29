712b38940df3eab96731628666dca5109d9e8a58
3litra <efanovalva@gmail.com>
2020-10-29 15:42:42 +0500

    New task04

--- src/main/java/ru/unnopolis/university/homework/task01/Spisok/SortContract.java
+++
@@ -1,8 +0,0 @@
-package ru.unnopolis.university.homework.task01.Spisok;
-
-/**
- * Интерфейс для переопределения метода sort в методах сортировки
- */
-public interface SortContract {
-    void sort(Person[] massiv) throws PersonException;
-}
